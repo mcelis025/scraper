3f64a96bc2c4f0e1b02c7a4827bbe5da946db0a6
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-07-11 09:11:01 -0700

    added css and image

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 var express = require("express");
 var exphbs  = require('express-handlebars');
-
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-// Require all models
 var db = require("./models");
-
 var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
+// Middleware
 app.use(logger("dev"));
-// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
 
 // Routes
+
+// Handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
